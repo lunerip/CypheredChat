bbbc861e674f59ab41fd0c2ed35134d928082b35
Luis Enrique Neri <A01745995@itesm.mx>
2019-10-04 12:14:23 -0500

    First Update:
    Can send messages

---
+++ echo-client.py
@@ -0,0 +1,12 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 65432
+string = input("Escribe el mensaje para Neri")
+mess = bytes(string, 'utf-8')
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(mess)
+    data = s.recv(1024)
+
+print('Received ', data.decode('utf8'))
